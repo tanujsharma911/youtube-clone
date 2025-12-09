05db51fefdb7adde6b2f98a64f1bc22371bdb439
Tanuj Sharma <hellotanujsharma@gmail.com>
2025-12-09 23:39:25 +0530

    added server url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://youtube-clone-7ozu.onrender.com",
         changeOrigin: true,
       },
     },
