9baf5ad73e900e31f9f2ae28c65d1f05d24465dc
Tanuj Sharma <hellotanujsharma@gmail.com>
2025-12-11 00:08:16 +0530

    some more changes

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://youtube-clone-7ozu.onrender.com/api",
+  baseURL: "/api",
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: "https://youtube-clone-7ozu.onrender.com/api",
+  baseURL: "/api",
   withCredentials: true,
 });
 
